ad2b68326fe59d4cc1fae82113414a3c96ac9169
Shatki <Shatki@mail.ru>
2019-10-31 09:39:23 +0300

    first fetch

--- src/index.js
+++ src/index.js
@@ -6,28 +6,32 @@ import App from './App';
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 class EvotorService {
-    _apiBase = 'https://evotorservice.ru/api/v1/';
-    //_apiBase = 'http://127.0.0.1:8000/api/v1/';
+    //_apiBase = 'https://evotorservice.ru/api/v1/';
+    _apiBase = 'http://127.0.0.1:8000/api/v1/';
     async getResource(url) {
         const data = {
-            //method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Authentication': 'a06c4306-732d-4914-9543-a588af06c683',
-                'user_id': '01-000000000738894',
-                'Content-Type': 'application/json;charset=utf-8'
-            }
+            user_id: '01-000000000738894',
         };
 
-        const response = await fetch(`${this._apiBase}${url}/`);
+        const headers = new Headers({
+            'Content-Type': 'application/json;charset=utf-8',
+            //Authentication: 'a06c4306-732d-4914-9543-a588af06c683',
+        });
+
+        const response = await fetch(`${this._apiBase}${url}/`, {
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify(data),
+            headers: headers
+        });
 
         // получить один заголовок
         // alert(response.headers.get('Content-Type')); // application/json; charset=utf-8
 
         // перебрать все заголовки
-        for (let [key, value] of response.headers) {
-            alert(`${key} = ${value}`);
-        }
+        //for (let [key, value] of response.headers) {
+        //    alert(`${key} = ${value}`);
+        //}
 
 
         if(!response.ok){
