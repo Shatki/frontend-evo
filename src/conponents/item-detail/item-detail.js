b3381f7b16677d6f3a512b5ad4f780af8011f5e6
Shatki <Shatki@mail.ru>
2019-12-23 09:38:42 +0300

    Добавлен диалог редактирования кодов ComboBox у ItemList

--- src/conponents/item-detail/item-detail.js
+++ src/conponents/item-detail/item-detail.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import { DataGrid, GridColumn } from 'rc-easyui'
+import { DataGrid, GridColumn, Dialog, LinkButton } from 'rc-easyui'
 import { NumberBox, TextBox, SwitchButton, ComboBox } from  'rc-easyui'
 import ContextMenu from "../context-menu";
-import './item-detail.css'
 import ErrorBoundry from "../error-boundry";
-
+import './item-detail.css'
 
 export default class ItemDetail extends Component {
     constructor(props) {
@@ -37,7 +36,9 @@ export default class ItemDetail extends Component {
             ],
             itemData : [],
             data: [],
-            editingRow: null
+            editingRow: null,
+            comboDlgTitle: "Редактирование кодов",
+            comboDlgClosed: true
         };
     }
 
@@ -46,8 +47,89 @@ export default class ItemDetail extends Component {
         //this.setState({ itemProps.valueField: value })}
     };
 
-    handleClickComboValueChange =(e) => {
-        console.log(e, this.comboData);
+    handleClickComboValueChange = (row) => {
+        //this.setState();
+        console.log(row);
+    };
+
+    updateData = () => {
+        const { itemData } = this.props;
+        // Преобразование для групповых свойств
+        // Todo: тут кривой временный код
+        return this.state.itemProps.map((property) => {
+            if(itemData !== null){
+                // Todo: доработать: Если данных нет null или [], то пробуем взять их из data
+                if (property["editorField"] !== "combo")
+                    property.valueField = itemData[property.nameField];
+                //console.log(property);
+            }
+            return property;
+        })
+    };
+
+    handleItemContextMenu = ({ row, column, originalEvent }) => {
+        originalEvent.preventDefault();
+        console.log(row.nameField);
+        this.menu.showContextMenu(originalEvent.pageX, originalEvent.pageY)
+    };
+
+    handleItemClick = (value) => {
+        console.log(value);
+    };
+
+    renderComboEditDialog = () => {
+        const { comboDlgTitle, comboDlgClosed } = this.state;
+        return(
+            <Dialog
+                title= { comboDlgTitle }
+                closed = { comboDlgClosed }
+                style={{ width: 400, height: 200 }}
+                bodyCls="f-column"
+                modal
+                onClose={() => this.setState({ comboDlgClosed: true })}
+                ref={ref => this.dlg = ref}
+            >
+                <div className="dialog-toolbar">
+                    <LinkButton iconCls="icon-edit" plain>Edit</LinkButton>
+                    <LinkButton iconCls="icon-help" plain>Help</LinkButton>
+                </div>
+                <div className="f-full">
+                    <p style={{ textAlign: 'center', margin: '20px 0', fontSize: '16px' }}>The Dialog Content.</p>
+
+
+                </div>
+                <div className="dialog-button">
+                    <LinkButton style={{ width: 80 }}>Сохранить</LinkButton>
+                    <LinkButton
+                        onClose={() => this.setState({ comboDlgClosed: true })}
+                        style={{ width: 80 }}>Отмена</LinkButton>
+                </div>
+            </Dialog>
+        )
+    };
+
+    renderView = ({ row }) => {
+        if (row.editorField === "text")
+            return(<div>{ row.valueField }</div>);
+        else if (row.editorField === "number")
+            return(<div>{ row.valueField }</div>);
+        else if (row.editorField === "switch")
+            return(<SwitchButton value={row.valueField}/>);
+        else if (row.editorField === "combo") {
+                let combo = row.valueField.find(item => (item.value === row.valueField));
+                if (combo === undefined)
+                // Todo: Ошибка в данных. нужна валидация
+                    combo = row.valueField[0];
+                return(<div>{ combo.text }</div>);
+        }
+        else if (row.editorField === "combo_add") {
+                if(row.valueField !== undefined && row.valueField.length > 0){
+                    return(<div>{ row.valueField[0] }</div>);
+                }
+                else{
+                    return(<div/>);
+                }
+        }
     };
 
     renderGroup = ({ value, rows }) =>{
@@ -79,60 +161,11 @@ export default class ItemDetail extends Component {
                         <span
                             className="textbox-icon icon-evotor-edit"
                             title="Править коды"
-                            onClickCapture={ this.handleClickComboValueChange }>
+                            onClick = { ()=>this.handleClickComboValueChange(row) }>
                         </span>
                 )}
             />);
         }
-    };
-
-    renderView = ({ row }) => {
-        if (row.editorField === "text")
-            return(<div>{ row.valueField }</div>);
-        else if (row.editorField === "number")
-            return(<div>{ row.valueField }</div>);
-        else if (row.editorField === "switch")
-            return(<SwitchButton value={row.valueField}/>);
-        else if (row.editorField === "combo") {
-                let combo = row.valueField.find(item => (item.value === row.valueField));
-                if (combo === undefined)
-                // Todo: Ошибка в данных. нужна валидация
-                    combo = row.valueField[0];
-                return(<div>{ combo.text }</div>);
-        }
-        else if (row.editorField === "combo_add") {
-                if(row.valueField !== undefined && row.valueField.length > 0){
-                    return(<div>{ row.valueField[0] }</div>);
-                }
-                else{
-                    return(<div/>);
-                }
-        }
-    };
-
-    updateData = () => {
-        const { itemData } = this.props;
-        // Преобразование для групповых свойств
-        // Todo: тут кривой временный код
-        return this.state.itemProps.map((property) => {
-            if(itemData !== null){
-                // Todo: доработать: Если данных нет null или [], то пробуем взять их из data
-                if (property["editorField"] !== "combo")
-                    property.valueField = itemData[property.nameField];
-                    //console.log(property);
-            }
-            return property;
-        })
-    };
-
-    handleItemContextMenu = ({ row, column, originalEvent }) => {
-        originalEvent.preventDefault();
-        console.log(row.nameField);
-        this.menu.showContextMenu(originalEvent.pageX, originalEvent.pageY)
-    };
-
-    handleItemClick = (value) => {
-        console.log(value);
     };
 
     renderContextMenu = (menu) => {
@@ -168,6 +201,7 @@ export default class ItemDetail extends Component {
                     />
                 </DataGrid>
                 { this.renderContextMenu(this.props.contextMenu) }
+                { this.renderComboEditDialog() }
             </ErrorBoundry>
         )
 
